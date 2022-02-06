7e9d21814f6983e75b868825b8887b96a4f42f2f
Phil Eaton <phil@eatonphil.com>
2022-02-06 17:03:26 -0500

    Update sqlite gifs for quality

--- internal/gifify.sh
+++ internal/gifify.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-ffmpeg -i "$1" -filter:v "setpts=0.5*PTS" "$2"
+ffmpeg -i "$1" -vf "setpts=0.5*PTS,fps=16,split[s0][s1];[s0]palettegen=max_colors=128:reserve_transparent=0[p];[s1][p]paletteuse" "$2"
